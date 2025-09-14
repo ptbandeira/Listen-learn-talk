9965dd617c3a96bfbe37b8e04b28f0b0e5b2a90f
ptbandeira <ptbandeira@gmail.com>
2025-09-14 19:38:59 +0000

    devnix fix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,10 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
+  # Which nixpkgs channel to use.
   channel = "stable-24.05";
 
+  # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.python311
     pkgs.python311Packages.pip
@@ -8,6 +12,7 @@
     pkgs.nodePackages.nodemon
   ];
 
+  # Sets environment variables in the workspace
   env = {};
 
   idx = {
@@ -18,19 +23,15 @@
 
     previews = {
       enable = true;
-      previews = [
-        {
-          id = "web";
-          port = 8000;
+      previews = {
+        web = {
           command = [ "python3" "-m" "http.server" "8000" ];
           manager = "web";
-        }
-        {
-          id = "backend";
-          port = 3000;
+        };
+        backend = {
           command = [ "npm" "run" "dev" "--prefix" "backend" ];
-        }
-      ];
+        };
+      };
     };
   };
 }
