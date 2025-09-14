6aa438010cf809b387ffc3bab12c5ba528e6c535
ptbandeira <ptbandeira@gmail.com>
2025-09-14 18:35:28 +0000

    nix update fix syntax

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,40 +1,36 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-24.05";
 
-  # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
     pkgs.python311
     pkgs.python311Packages.pip
     pkgs.nodejs_20
     pkgs.nodePackages.nodemon
   ];
 
-  # Sets environment variables in the workspace
   env = {};
+
   idx = {
+    extensions = [
+      "vscode.git"
+      "github.copilot"
+    ];
+
     previews = {
       enable = true;
       previews = [
         {
           id = "web";
           port = 8000;
-          command = ["python3", "-m", "http.server", "8000"];
+          command = [ "python3" "-m" "http.server" "8000" ];
           manager = "web";
-        },
+        }
         {
           id = "backend";
           port = 3000;
-          command = ["npm", "run", "dev", "--prefix", "backend"];
+          command = [ "npm" "run" "dev" "--prefix" "backend" ];
         }
       ];
     };
-    extensions = [
-      "vscode.git",
-      "github.copilot",
-    ];
   };
 }
